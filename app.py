8087a14249213b45e0bd8db2da7c2f70fedaa4c8
D1N2O3B4 <davebostons2017@gmail.com>
2023-05-27 23:35:49 +0100

    modified app.py

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-user_choice = input("Type add,show,remove or press enter to quit")
 store = []
 
 def add(params):
     store.append(params)
-
-    
 def show():
     for i in store.index():
         print(f"{i}")
 
-
+        
 def remove(params):
     store.remove(params)
 
+operations = {
+    "add":add,
+    "show":show,
+    'remove':remove
+}
+
+user_choice = input("Type add,show,remove or press enter to quit")
+
+if operations[user_choice] != "":
+    pass
+
+else:
+    pass
