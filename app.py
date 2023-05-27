ef23ccb627eaa1eaed1e4cdde47b0b562afa6f5b
D1N2O3B4 <davebostons2017@gmail.com>
2023-05-27 23:59:01 +0100

    fixed dictionary error

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ store = []
 
 def add(params):
     store.append(params)
+
 def show():
     print(store)
 
@@ -17,10 +18,12 @@ operations = {
 
 user_choice = input("Type add,show,remove or press enter to quit")
 
-while operations[user_choice] != "":
+while user_choice != "":
     if user_choice == "add":
         action = input("Type in action")
         operations[user_choice](action)
         user_choice = input("Type add,show,remove or press enter to quit")
     else:
+        print("wORKING")
         operations[user_choice]
+        user_choice = input("Type add,show,remove or press enter to quit")
