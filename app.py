f2e4af4699b978524708ba4ada0a0b272d649df0
D1N2O3B4 <davebostons2017@gmail.com>
2023-05-28 17:56:12 +0100

    modifed app.py

--- app.py
+++ app.py
@@ -4,25 +4,27 @@ def add(params):
     store.append(params)
 
 def show():
-    print(store)
+    index = 0
+    for index,action in enumerate(store,start=1):
+        print(f"{index}.{action}")
 
         
 def remove(params):
     store.remove(params)
 
-operations = {
-    "add":add,
-    "show":show,
-    'remove':remove
-}
 
-user_choice = input("Type add,show,remove or press enter to quit")
+user_choice = input("Type add,show,remove or press enter to quit\n")
 
 while user_choice != "":
     if user_choice == "add":
-        action = input("Type in action")
-        operations[user_choice](action)
-        user_choice = input("Type add,show,remove or press enter to quit")
+        action = input("Type in action\n")
+        add(action)
+        user_choice = input("Type add,show,remove or press enter to quit\n")
     else:
-        operations[user_choice]
-        user_choice = input("Type add,show,remove or press enter to quit")
+        if user_choice == "show":
+            show()
+            user_choice = input("Type add,show,remove or press enter to quit\n")
+        else:
+            action = input("Type in action to remove\n")
+            remove(action)
+            user_choice = input("Type add,show,remove or press enter to quit\n")
