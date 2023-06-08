3d8263fc8c550f047bd6681ed0f4f110decb8a65
D1N2O3B4 <davebostons2017@gmail.com>
2023-06-08 06:00:23 +0000

    added custom func listify

--- tenary_ops.py
+++ tenary_ops.py
@@ -144,7 +144,11 @@ url1 = 'https://gist.githubusercontent.com/aakashns/257f6e6c8719c17d0e498ea287d1
 
 urlretrieve(url1,"./Created/read.txt")
 
+def listify(values):
+    return values.strip().split(",")
+
 # # r = request.get('https://api.github.com/user', auth=('user', 'pass'))
 with open("./Created/read.txt","r") as file2:
     # print(file2.read())
-    print(file2.readlines())
+    read_f = file2.readlines()
+    print(listify(read_f[0]))
