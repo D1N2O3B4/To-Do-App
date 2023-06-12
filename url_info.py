cf05102bdc8eafc3fb1a88fd949548d1319ca7ec
D1N2O3B4 <davebostons2017@gmail.com>
2023-06-12 22:08:26 +0000

    added url_info.py

---
+++ url_info.py
@@ -0,0 +1,5 @@
+from urllib.request import urlretrieve
+
+url_1 = "https://gist.githubusercontent.com/aakashns/257f6e6c8719c17d0e498ea287d1a386/raw/7def9ef4234ddf0bc82f855ad67dac8b971852ef/loans1.txt"
+
+urlretrieve(url_1,"./Created/url.txt")
