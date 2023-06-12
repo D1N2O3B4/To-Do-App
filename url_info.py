7f5d3f01dd7257ec4d979c1b9b4943e048811e39
D1N2O3B4 <davebostons2017@gmail.com>
2023-06-12 22:19:40 +0000

    added comment

--- url_info.py
+++ url_info.py
@@ -1,5 +1,5 @@
 from urllib.request import urlretrieve
 
-url_1 = "https://gist.githubusercontent.com/aakashns/257f6e6c8719c17d0e498ea287d1a386/raw/7def9ef4234ddf0bc82f855ad67dac8b971852ef/loans1.txt"
+url_1 = "https://www.kaggle.com/datasets/paultimothymooney/breast-histopathology-images/download?datasetVersionNumber=1"
 
-urlretrieve(url_1,"./Created/url.txt")
+urlretrieve(url_1,"./Created/url.txt")#retireves only web content not files
