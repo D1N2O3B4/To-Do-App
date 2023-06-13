5cf9bc83d552f726061159ad2b636359e0ca0b2b
D1N2O3B4 <davebostons2017@gmail.com>
2023-06-13 21:52:00 +0000

    able to code read_csv() func

--- build.py
+++ build.py
@@ -1,23 +1,50 @@
-class Car:
-    def __init__(self,brand,version,year,c_speed,hp,petrol_cap):
-        self.brand = brand
-        self.version = version
-        self.year = year
-        self.hp = hp
-        self.speed = c_speed
-        self.petrol_cap = petrol_cap
+# class Car:
+#     def __init__(self,brand,version,year,hp,petrol_cap):
+#         self.brand = brand
+#         self.version = version
+#         self.year = year
+#         self.hp = hp
+#         self.speed = 0
+#         self.petrol_cap = petrol_cap
     
-    def drive(self):
-        while (self.petrol_cap > 0):
-            if (self.speed != self.hp):
-                self.speed += 1
-            else:
-                print("Limit reached")
-            self.petrol_cap -= 20
-            return f"Vroom speed {self.speed}"
+#     def drive(self,distance):
+#         while (self.speed < self.hp):
+#             self.speed += 1
+#             print(f"increasing speed {self.speed} mph")
+        
+#         # while distance > 0:
 
 
 
-mycar = Car("Ferrari","Nuelo",2001,123,700,1000)
 
-print(Car.drive(mycar))
+# mycar = Car("Ferrari","Nuelo",2001,700,1000)
+
+# print(Car.drive(mycar,30))
+
+
+def split_headers(object):
+    return object.strip().split(",")
+
+def split_values(object):
+    new_values = []
+    for i in object.strip().split(","):
+        try:
+            new_values.append(float(i))
+        except ValueError:
+            new_values.append(0.0)
+    return new_values
+
+def dictyfy(val,head):
+    return dict(zip(head,val))
+
+final = []
+with open("./Created/read.txt","r") as r:
+    p = r.readlines()
+headers = p[0]
+arranged_headers = split_headers(headers)
+values = p[1:]
+for i in values:
+    arranged_values = split_values(i)
+    end = dictyfy(arranged_values,arranged_headers)
+    final.append(end)
+print(final)
