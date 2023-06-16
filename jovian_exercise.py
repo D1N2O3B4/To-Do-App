06aecfa70b970902d1d979d78cd39eca6326142f
D1N2O3B4 <davebostons2017@gmail.com>
2023-06-16 05:15:15 +0000

    fixed "" problem

--- jovian_exercise.py
+++ jovian_exercise.py
@@ -8,49 +8,30 @@ urlretrieve(url1,"./Created/read1.txt")
 urlretrieve(url2,"./Created/read2.txt")
 urlretrieve(url3,"./Created/read3.txt")
 
-
-
-
-
 def header_split(obj):
   return obj.strip().split(",")
 
-def values_split(obj):
-  value_list = []
+def to_list(obj):
+  new_values = []
   for i in obj.strip().split(","):
+    if i[::-1] != "":
+      new_values.append(i)
+    else:
+      new_values.append(0.0)
+  return new_values
+    
 
-    try:
-        value_list.append(float(i))
-    except ValueError:
-        value_list.append(0.0)
-    return value_list
 
-final = []
-with open("./Created/read.txt","r") as r:
-    p = r.readlines()
-headers = p[0]
-arranged_headers = split_headers(headers)
-values = p[1:]
+dicty = {}
+with open("./Created/read1.txt","r") as f:
+  fil = f.readlines()
+  headers = header_split(fil[0])
+  values = fil[1:]
+ 
+  for value in values:
+    print(to_list(value))
 
-for i in values:
-    arranged_values = split_values(i)
-    end = dictyfy(arranged_values,arranged_headers)
-    final.append(end)
-print(final)
 
-def read_csv_columnar(path):
-  with open(path,"r") as f:
-    read_f = f.readlines()
-    headers = read_f[0]
-    values = read_f[1:]
-    ultimate = {}
-    good_vals = values_split(values)
-    good_headers = header_split(headers)
-    print(good_headers)
-    print(good_vals)
-
-    # for i in good_headers:
-    #   ultimate[i] = 
-
-read_csv_columnar("./Created/read1.txt")
-
+  # for header in headers:
+  #   print(header)
+  #   dicty[header] = 0
