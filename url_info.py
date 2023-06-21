29173812feb10c713f585eb9f0b44a6dcb69e395
D1N2O3B4 <davebostons2017@gmail.com>
2023-06-21 22:41:34 +0000

    trying out numpy arrays and functions

--- url_info.py
+++ url_info.py
@@ -1,5 +1,18 @@
-from urllib.request import urlretrieve
+import numpy as np
+# from urllib.request import urlretrieve
 
-url_1 = "https://www.kaggle.com/datasets/paultimothymooney/breast-histopathology-images/download?datasetVersionNumber=1"
+# url_1 = "https://www.kaggle.com/datasets/paultimothymooney/breast-histopathology-images/download?datasetVersionNumber=1"
 
-urlretrieve(url_1,"./Created/url.txt")#retireves only web content not files
+# urlretrieve(url_1,"./Created/url.txt")#retireves only web content not files
+
+a = [19,23,34]
+b = [90,15,12]
+
+a = np.array(a)
+b = np.array(b)
+
+print(a)
+print(b)
+
+print(np.matmul(a,b))
+print(a @ b)
