10732092d07f5341882b32514d8df881fffdd357
d1n2o3b4 <davebostons2017@gmail.com>
2023-08-17 20:20:59 +0100

    modified app.py

--- app.py
+++ app.py
@@ -1,36 +1,36 @@
-# import os
-# store = []
+import os
+store = []
 
-# def add(params):
-#     store.append(params)
+def add(params):
+    store.append(params)
 
-# def show():
-#     index = 0
-#     for index,action in enumerate(store,start=1):
-#         print(f"{index}.{action}")
+def show():
+    index = 0
+    for index,action in enumerate(store,start=1):
+        print(f"{index}.{action}")
 
         
-# def remove(params):
-#     store.remove(params)
+def remove(params):
+    store.remove(params)
 
 
-# user_choice = input("Type add,show,remove or press enter to quit\n")
+user_choice = input("Type add,show,remove or press enter to quit\n")
 
-# while user_choice != "":
-#     if user_choice == "add":
-#         action = input("Type in action\n")
-#         add(action)
-#         user_choice = input("Type add,show,remove or press enter to quit\n")
+while user_choice != "":
+    if user_choice == "add":
+        action = input("Type in action\n")
+        add(action)
+        user_choice = input("Type add,show,remove or press enter to quit\n")
      
-#     else:
-#         if user_choice == "show":
-#             show()
-#             user_choice = input("Type add,show,remove or press enter to quit\n")
+    else:
+        if user_choice == "show":
+            show()
+            user_choice = input("Type add,show,remove or press enter to quit\n")
        
-#         else:
-#             action = input("Type in action to remove\n")
-#             remove(action)
-#             user_choice = input("Type add,show,remove or press enter to quit\n")
+        else:
+            action = input("Type in action to remove\n")
+            remove(action)
+            user_choice = input("Type add,show,remove or press enter to quit\n")
    
 title = input("Type in title")
 
