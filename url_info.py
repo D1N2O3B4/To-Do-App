94f854aaf6efaaf0368d9302203b01ce6de81a08
d1n2o3b4 <davebostons2017@gmail.com>
2023-08-17 20:21:35 +0100

    made changes here also

--- url_info.py
+++ url_info.py
@@ -1,18 +1,15 @@
-import numpy as np
-# from urllib.request import urlretrieve
+# import numpy as np
+# # from urllib.request import urlretrieve
 
-# url_1 = "https://www.kaggle.com/datasets/paultimothymooney/breast-histopathology-images/download?datasetVersionNumber=1"
+# # url_1 = "https://www.kaggle.com/datasets/paultimothymooney/breast-histopathology-images/download?datasetVersionNumber=1"
 
-# urlretrieve(url_1,"./Created/url.txt")#retireves only web content not files
+# # urlretrieve(url_1,"./Created/url.txt")#retrieves only web content not files
 
-a = [19,23,34]
-b = [90,15,12]
+# a = [3,4]
+# b = [1,2,3]
+# a = np.array(a)
+# b = np.array(b)
+# c = np.random
+# print
+print(len(input("Book title\n").replace(" ","")))
 
-a = np.array(a)
-b = np.array(b)
-
-print(a)
-print(b)
-
-print(np.matmul(a,b))
-print(a @ b)
