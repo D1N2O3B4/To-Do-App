5e6a19c15cf10ac4f3d72a1e17dc0dde980787ac
d1n2o3b4 <davebostons2017@gmail.com>
2023-08-17 20:58:11 +0100

    added a default case

--- switch.py
+++ switch.py
@@ -1,18 +1,21 @@
 user_input = input("Type in any day of the week:\n").capitalize()
 
 match user_input:
-    case "Monday":
-        print("Garfield hates Mondays")
+    case "Monday"| "Manday":
+        print("Garfield hates Mondays. Fun fact i used to spell Monday Manday")
     case "Tuesday":
         print("Tuesdays are ok")
     case "Wednesday":
         print("Middle week day wednesday")
     case "Thursday":
         print("Mokuyoubi is Thursday in Japanese")
-    case "Friday":
-        print("Kinyoubi is friday in Japanese")
+    case "Friday" | "Fryday":
+        print("Kinyoubi is friday in Japanese. Fun fact i used to spell Friday Fryday")
     case "Saturday":
         print("This day is for fun")
     case "Sunday":
         print("Sunday is always sunny")
+    #This gets called if no case matches your input any var or approved char can be used. eg: name,_name,defk etc
+    case _:
+        print("Your input doesn't match any case")
     
