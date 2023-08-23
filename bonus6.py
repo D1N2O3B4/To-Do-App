02c973e4df3eb5f752600c24fd43bb68a3a9b47a
d1n2o3b4 <davebostons2017@gmail.com>
2023-08-23 13:42:39 +0100

    made some changes

--- bonus6.py
+++ bonus6.py
@@ -2,6 +2,12 @@ content = ["lorem ispum","Marksis","Denifatetley"]
 
 filenames = ["doc.txt","report.txt","presentation.txt"]
 
-for i in range(len(content)):
-    with open(filenames[i],"w") as f:
-        f.writelines(content[i])
+# for i in range(len(content)):
+#     with open(filenames[i],"w") as f:
+#         f.writelines(content[i])
+
+#OR
+
+for c,f in zip(content,filenames):
+    with open(f,"w") as file:
+        file.writelines(c)
--- todo.py
+++ todo.py
@@ -16,10 +16,22 @@ while quitOpt:
                 f.writelines(todo)
         case "edit":
             editTodo = input("Input Todo number to edit:\n")
+            index = int(editTodo) - 1
+            newTodo = input("Please type in new Todo:") +"\n"
+            # with open("todo.txt","rw") as f:
+            #     old = f.readlines()
+            #     old[index] = newTodo
+            with open("todo.txt","r") as read:
+                old = read.readlines()
+            
+            with open("todo.txt","w") as write:
+                old[index] = newTodo
+                write.writelines(old)
             
         case "show":
             with open("todo.txt","r") as f:
-                for i,j in enumerate(f.readlines(),start=1):
+                #Yeah i know the list comprehension is too much to handle eh
+                for i,j in enumerate([i.strip("\n") for i in f.readlines()],start=1):
                     print(f"{i}.{j.strip('n')}")
             
         case "complete":
