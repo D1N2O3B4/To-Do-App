025a9ff4b59c93fcac5ddb3063e2c603c13629b3
d1n2o3b4 <davebostons2017@gmail.com>
2023-08-23 22:18:55 +0100

    modified the complete case

--- todo.py
+++ todo.py
@@ -18,9 +18,7 @@ while quitOpt:
             editTodo = input("Input Todo number to edit:\n")
             index = int(editTodo) - 1
             newTodo = input("Please type in new Todo:") +"\n"
-            # with open("todo.txt","rw") as f:
-            #     old = f.readlines()
-            #     old[index] = newTodo
+        
             with open("todo.txt","r") as read:
                 old = read.readlines()
             
@@ -32,10 +30,17 @@ while quitOpt:
             with open("todo.txt","r") as f:
                 #Yeah i know the list comprehension is too much to handle eh
                 for i,j in enumerate([i.strip("\n") for i in f.readlines()],start=1):
-                    print(f"{i}.{j.strip('n')}")
+                    print(f"{i}.{j}")
             
         case "complete":
-            pass
+            completeTodo = int(input("Input Todo number to complete:\n"))
+            with open("todo.txt","r") as f:
+                lisp = f.readlines()
+            vartodo = lisp[completeTodo -1]
+            lisp.pop(completeTodo-1)
+            with open("todo.txt","w") as f:
+                f.writelines(lisp)
+            print(f"{vartodo} was removed")
         case "quit":
             quitOpt = False
         case _:
