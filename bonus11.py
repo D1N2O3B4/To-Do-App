6376306c55fadc3e465eb29f585965ee34306436
d1n2o3b4 <davebostons2017@gmail.com>
2023-08-30 22:49:34 +0100

    added function

---
+++ bonus11.py
@@ -0,0 +1,10 @@
+def get_average():
+    with open("num.txt","r") as f:
+        nums = f.readlines()
+    value = nums[1:]
+    value = [float(i) for i in value]
+    average_local = 
+    return ()
+
+average = get_average()
+print(average)
