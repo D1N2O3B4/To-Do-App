232d21c7b06d34d118a1efdca5d750d61789a641
d1n2o3b4 <davebostons2017@gmail.com>
2023-08-30 22:54:16 +0100

    fixed error

--- bonus11.py
+++ bonus11.py
@@ -1,7 +1,8 @@
 def get_average():
-    with open("num.txt","r") as f:
-        nums = f.readlines()
-    value = nums[1:]
+    with open("fip.txt","r") as f:
+        lisp = f.readlines()
+        print(lisp)
+    value = lisp[1:]
     value = [float(i) for i in value]
     average_local = sum(value)/len(value)
     return average_local
