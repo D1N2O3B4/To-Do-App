881afff46ecac5d28975352b4a4f378064016289
d1n2o3b4 <davebostons2017@gmail.com>
2023-09-02 21:07:16 +0100

    decoupled a function

--- bonus12.py
+++ bonus12.py
@@ -1,11 +1,16 @@
 """FEET TO METRES CONVERTER"""
 feetVal = input("Type feet followed by a \" then the inches of your height: ")
 
-def convertToMetres(feetVal):
+
+def parse():
     lisp = feetVal.split('"')
     feet = float(lisp[0])
     inches = float(lisp[1])
+    return {'feet':feet,'inches':inches}
+
+def convertToMetres(feet,inches):
     metres = feet*.3048 + inches*.0254
-    return f"{lisp[0]}\"{lisp[1]} is {metres} in metres"
+    return f"{int(feet)}\"{int(inches)} is {metres} in metres"
     
-print(convertToMetres(feetVal))
+parsed = parse()
+print(convertToMetres(parsed['feet'],parsed['inches']))
